2284a96600c610fd12d4b9da212702de590ef6c0
Rohit Nandi <rohitnandi12@gmail.com>
2019-06-16 19:23:50 +0530

    Word entered in popup.html is read, and list of words saved in db diplayed as list.

--- popup/popup.js
+++ popup/popup.js
@@ -1,20 +1,40 @@
 
-let changeColor = document.getElementById('changeColor');
-
-chrome.storage.sync.get('color', function(data){
-    changeColor.style.backgroundColor = data.color;
-    changeColor.setAttribute('value', data.color);
-});
-
-
-changeColor.onclick = function(element){
-
-    let color = element.target.value;
-    chrome.tabs.query({active: true, currentWindow:true}, function(tabs){
-        chrome.tabs.executeScript(
-            tabs[0].id,
-            {code: 'document.body.style.backgroundColor = "'+color+'";'}
-
-        );
-    });
+var refresh_list = function () {
+    let displayStoredWords = document.getElementById('displayStoredWords');
+    // These words will come from db
+    let words = ['aone','ctwo','bthree'];
+    
+    for(i=0; i<words.length; i++){
+        console.log(words[i])
+        let li = document.createElement('li');
+        li.append(words[i]);
+        displayStoredWords.appendChild(li);
+    }
+    
 };
+
+let submitWord = document.getElementById('submitWord');
+
+// chrome.storage.sync.get('color', function(data){
+//     changeColor.style.backgroundColor = data.color;
+//     changeColor.setAttribute('value', data.color);
+// });
+
+console.log(submitWord);
+refresh_list();
+submitWord.onclick = function (element) {
+
+    let word = document.getElementById('wordInput').value;
+    // chrome.tabs.query({active: true, currentWindow:true}, function(tabs){
+    //     chrome.tabs.executeScript(
+    //         tabs[0].id,
+    //         {code: 'document.body.style.backgroundColor = "'+color+'";'}
+
+    //     );
+    // });
+    console.log("submitted word is "+word);
+    // word has to be store in db.
+    refresh_list();
+};
+
+
