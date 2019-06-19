536aaae65828633b276038f9f1624232c1958bf6
Rohit Nandi <rohitnandi12@gmail.com>
2019-06-19 14:32:10 +0530

    Storage module , insertWord , basic wordName persistances.

---
+++ background/background.js
@@ -0,0 +1,27 @@
+/****************************************
+Module which handles the persistance related functionalities.
+*************************************** */
+var storage_module = (function(){
+
+    //word_data = {word,definition, url}
+    var _insertWord = function(word){
+        
+        chrome.storage.sync.get(word,function(result){
+            // check if word already exists
+            if($.isEmptyObject(result)){
+                // Add the word
+                let wordObj = {};
+                wordObj[word] = word;
+                chrome.storage.local.set(wordObj, function(){
+                    console.log("Word "+word+" successfully saved");
+                });
+            }else{
+                console.log("The word "+word+" already exists");
+            }
+        });       
+    }
+    return {
+        insertWord : _insertWord
+    }
+})();
+
